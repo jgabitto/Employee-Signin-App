2c6a9da977bc67c7505b2bce73bf1da043106f9e
Jorge Gabitto <jgabitto1792@gmail.com>
2020-02-04 13:42:21 -0600

    Change from 2 to 1 databases and add mLab database name to url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,28 +1,41 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// let url1 = `${process.env.MONGODB_URI}/heroku_brtwtffk` || 'mongodb://127.0.0.1:27017/employee-sign-in-login';
+// let url2 = `${process.env.MONGODB_URI}/heroku_brtwtffk` || 'mongodb://127.0.0.1:27017/employee-sign-in-records';
 
-// mongoose.connect('mongodb://127.0.0.1:27017/employee-sign-in', {
+
+// // mongoose.connect('mongodb://127.0.0.1:27017/employee-sign-in', {
+// //     useNewUrlParser: true,
+// //     useUnifiedTopology: true,
+// //     useCreateIndex: true,
+// //     useFindAndModify: false
+// // });
+
+// const conn1 = mongoose.createConnection(url1, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false
+// });
+
+// const conn2 = mongoose.createConnection(url2, {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 //     useCreateIndex: true,
 //     useFindAndModify: false
 // });
 
-const conn1 = mongoose.createConnection('mongodb://127.0.0.1:27017/employee-sign-in-login', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-});
 
-const conn2 = mongoose.createConnection('mongodb://127.0.0.1:27017/employee-sign-in-records', {
+// module.exports = {
+//     conn1,
+//     conn2
+// };
+
+const mongoose = require('mongoose');
+let url = 'mongodb://<dbuser>:<dbpassword>@ds061751.mlab.com:61751/heroku_brtwtffk' || 'mongodb://127.0.0.1:27017/employee-sign-in';
+
+mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
 });
-
-
-module.exports = {
-    conn1,
-    conn2
-};
--- src/models/record.js
+++ src/models/record.js
@@ -1,6 +1,74 @@
+// const mongoose = require('mongoose');
+// const validator = require('validator');
+// const { conn2 } = require('../db/mongoose');
+
+// const recordSchema = new mongoose.Schema({
+//     date: {
+//         type: Date,
+//         required: true,
+//         trim: true
+//     },
+//     firstName: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     lastName: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid');
+//             }
+//         }
+//     },
+//     dateIn: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     timeIn: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     timeOut: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     hoursWorked: {
+//         type: Number,
+//         required: true,
+//         trim: true
+//     }
+// })
+
+// recordSchema.statics.findByCredentials = async (dateIn, email) => {
+//     const record = await Record.findOne({ dateIn, email });
+
+//     if (record) {
+//         throw new Error('Record already exists for specified date and employee. Administrative approval is needed to submit new record for specified date and employee.');
+//     }
+    
+//     return record;
+// }
+
+
+// // const SignIn = mongoose.model('SignIn', signInSchema);
+// const Record = conn2.model('Record', recordSchema);
+
+// module.exports = Record;
+
 const mongoose = require('mongoose');
 const validator = require('validator');
-const { conn2 } = require('../db/mongoose');
 
 const recordSchema = new mongoose.Schema({
     date: {
@@ -63,6 +131,6 @@ recordSchema.statics.findByCredentials = async (dateIn, email) => {
 
 
 // const SignIn = mongoose.model('SignIn', signInSchema);
-const Record = conn2.model('Record', recordSchema);
+const Record = mongoose.model('Record', recordSchema);
 
 module.exports = Record;
